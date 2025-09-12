51b83619b43e3a60e06d4f4c968f099b66ba69ec
happySquidd <rafael.safin077@gmail.com>
2025-09-11 23:54:49 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace scheduleApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // get the connection string
+            string constr = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
+
+            // make the connection
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(constr);
+
+                conn.Open();
+                MessageBox.Show("Connected to db");
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //close the connection
+                if(conn != null) { conn.Close(); }
+            }
+        }
+    }
+}
